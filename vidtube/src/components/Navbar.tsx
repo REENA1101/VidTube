a88e21f004dfe67246e8e26e5cf93dc5e35508ed
Reena kumari <reenakvs1@gmail.com>
2024-01-05 23:51:26 +0530

    NAvbar begins

---
+++ vidtube/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center px-14 h-14 bg-[#21213] opacity-95 sticky top-0 z-50">Navbar</div>
+  )
+}
+
