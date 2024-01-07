2efb8a82f02b53708b277cb4e484a8b83ebd527c
Reena kumari <reenakvs1@gmail.com>
2024-01-07 13:44:16 +0530

    navbar all icons and notification placed navbar half complete

--- vidtube/src/components/Navbar.tsx
+++ vidtube/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi" 
-import { BsYoutube } from "react-icons/bs" 
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs" 
 import { Link } from 'react-router-dom'
-// import AiOutlineSearch from 
+import {AiOutlineSearch, AiOutlineClose} from 'react-icons/ai'
+import {TiMicrophone} from "react-icons/ti"
+import {IoAppsSharp} from "react-icons/io5"
 
 export default function Navbar() {
   return (
@@ -22,10 +24,32 @@ export default function Navbar() {
             <form action="">
                 <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
                     <div className="flex gap-4 items-center pr-5">
-                        {/* <AiOutlineSearch */}
+                        <div>
+                         <AiOutlineSearch  className="text-xl"/>
+                        </div>
+                        <input type="text" className="w-96 bg-zinc-900 focus:outline-none border-none"/>
+                       
+                           <AiOutlineClose className="text-xl cursor-pointer"/>
                     </div>
+                    <button className="h-10 w-16 flex items-center justify-center bg-zinc-800">
+                           <AiOutlineSearch  className="text-xl"/>
+                        </button>
                 </div>
             </form>
+
+            <div className="text-xl p-3 bg-zinc-900 rounded-full">
+                <TiMicrophone/>
+            </div>
+        </div>
+        <div className="flex gap-5 items-center text-xl">
+            <BsCameraVideo/>
+            <IoAppsSharp/>
+            <div className="relative">
+              <BsBell/>
+              <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+                9+
+              </span>
+            </div>
         </div>
     </div>
   )
