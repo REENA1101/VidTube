a8d6371f585562ed6d2aec8f8f5c2433a2b83040
Reena kumari <reenakvs1@gmail.com>
2024-01-07 15:13:09 +0530

    home component started

--- vidtube/src/pages/Home.tsx
+++ vidtube/src/pages/Home.tsx
@@ -3,8 +3,12 @@ import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div>
-        <Navbar/>
-        </div>
+    <div className="max-h-screen overflow-high">
+      <div style={{height: "7.5vh"}}>
+      <Navbar />
+      </div>
+
+      <div className='flex'></div>
+    </div>
   )
 }
