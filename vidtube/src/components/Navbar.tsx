821d6687ca5ff6c6daf38e9a98f16bc5969afe6c
Reena kumari <reenakvs1@gmail.com>
2024-01-07 15:01:29 +0530

    Navbar complete

--- vidtube/src/components/Navbar.tsx
+++ vidtube/src/components/Navbar.tsx
@@ -50,6 +50,9 @@ export default function Navbar() {
                 9+
               </span>
             </div>
+            <img src="https://yt3.ggpht.com/wgnEbc2Ec2JYkeyzUbiHzDlAFObI2Btwo2YRCEF1aCMBiRc5E_zWy8-URBQS3EMQ1yzzaGFR=s88-c-k-c0x00ffffff-no-rj-mo" 
+            className="w-9 h-9 rounded-full"
+            alt ="logo"/>
         </div>
     </div>
   )
