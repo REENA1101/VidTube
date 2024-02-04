c52142ded83ae580ed86f4ce37665e6d7bb81bce
Reena kumari <reenakvs1@gmail.com>
2024-02-04 13:01:30 +0530

    parseData convertrawViewstoString parseVideoDuration complete

--- vidtube/src/store/reducers/getHomePageVideos.ts
+++ vidtube/src/store/reducers/getHomePageVideos.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "..";
+import { parseData } from "../../utils";
 import axios from "axios";
 import { YOUTUBE_API_URL } from "../../utils/constants";
 
@@ -16,5 +17,5 @@ export const getHomePageVideos = createAsyncThunk(
     } = await axios.get(
         `${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`
         );
-        console.log(items);
+       const parsedData = await parseData(items)
     });
