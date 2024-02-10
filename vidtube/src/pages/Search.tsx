3810f000e9e11dfd5d1b063f4d8622cff70492cb
Reena kumari <reenakvs1@gmail.com>
2024-02-10 12:04:30 +0530

    Search Page created with error 400 on lvideo loading

--- vidtube/src/pages/Search.tsx
+++ vidtube/src/pages/Search.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import { HomePageVideos } from "../Types";
+import { clearVideos } from "../store";
+import { useNavigate } from "react-router-dom";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
 
 export default function Search() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate()
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if(searchTerm  === "") navigate("/");
+    else{
+      dispatch(getSearchPageVideos(false))
+    }
+  }, [dispatch, navigate, searchTerm]);
+
   return (
-    <div>Search</div>
-  )
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh", marginTop:"15px" }}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={600}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos, index: number) => {
+                return <Card data={item} key={`${item.videoId}_${index}`}/>;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 }
